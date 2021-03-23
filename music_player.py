8a4aa4a6c601ad610fd9a47aeb6d46d553217b7f
Harry Eldridge <heldridge@protonmail.com>
2021-03-23 10:27:02 -0400

    Add comments and type hints to music_player

--- music_player.py
+++ music_player.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import time
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
@@ -6,31 +7,41 @@ import pygame
 
 
 class NoSongPlayingError(Exception):
+    """Used when an operation is called expecting a playing song, but none exists"""
+
     pass
 
 
 class MusicPlayer:
+    """Handles the logic of loading and playing songs
+
+    Attributes:
+        playing_song: The path to the currently playing song
+        playing_song_length: The length of the currently playing song in seconds
+        playing_song_start_time:
+            The unix timestamp of when the current song started playing
+    """
+
     def __init__(self):
         pygame.init()
         self.reset_playing_song()
 
-    def reset_playing_song(self):
-        self.playing_song = None
-        self.playing_song_length = None
-        self.playing_song_start_time = None
+    def reset_playing_song(self) -> None:
+        self.playing_song: pathlib.Path = None
+        self.playing_song_length: float = None
+        self.playing_song_start_time: float = None
 
-    def play(self, song_path):
+    def play(self, song_path: pathlib.Path):
         pygame.mixer.music.load(song_path)
         pygame.mixer.music.play(0)
         self.playing_song = song_path
         self.playing_song_length = pygame.mixer.Sound(song_path).get_length()
         self.playing_song_start_time = time.time()
 
-    def is_playing(self):
+    def is_playing(self) -> bool:
         return pygame.mixer.music.get_busy()
 
-    def get_playing_song_percentage(self):
-
+    def get_playing_song_percentage(self) -> float:
         if not pygame.mixer.music.get_busy():
             self.reset_playing_song()
             raise NoSongPlayingError("No song is currently playing")
